145b923725965af24719a51e4b8b5dd0e1af9919
SURENDRAN <www.surendran059@gmail.com>
2026-09-14 20:30:01 +0530

    Add: duplicate-integer - submission-33

---
+++ Data Structures & Algorithms/duplicate-integer/submission-33.py
@@ -0,0 +1,26 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        # first is intalized for brute force apply i get is ok now answer
+        # is correct but time and space complexity is large for is come that rate limit o(n2) is so many time is get that
+
+        # now i refrom the time and space complexity for o(n), o(n)
+
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+
+
+
+
+
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
